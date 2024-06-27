00f62e93efae1f366b43a58d92143163f73c9ba3
Campbell <campbellrankine@avertro.com>
2024-06-27 12:19:23 +1000

    auth added

--- machinehub/app/(default)/page.tsx
+++ machinehub/app/(default)/page.tsx
@@ -3,11 +3,21 @@ export const metadata = {
   description: 'Page description',
 }
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import FeaturesBlocks from '@/components/features-blocks'
+import Hero from '@/components/hero';
+import Features from '@/components/features';
+import FeaturesBlocks from '@/components/features-blocks';
+
+import { auth } from "@/app/api/firebase/firebaseConfig";
+import { currentUser } from "@/components/ui/header";
+
 
 export default function Home() {
+
+  if (currentUser != undefined){
+    console.log(currentUser.displayName);
+    console.log(currentUser.email);
+  }
+
   return (
     <>
       <Hero />
--- machinehub/app/api/firebase/firebaseConfig.tsx
+++ machinehub/app/api/firebase/firebaseConfig.tsx
@@ -3,24 +3,26 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+require('dotenv').config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const { FIREBASE_API, FIREBASE_MESSAGE_ID, FIREBASE_APP_ID } = process.env
+const FIREBASE_API = process.env.REACT_APP_FIREBASE_API;
 
 export const firebaseConfig = {
-  apiKey: FIREBASE_API,
+  apiKey: "AIzaSyDrqnALkx0YyJP661E2xMKXR72WL37LK5c",
   authDomain: "mhub-752b1.firebaseapp.com",
   projectId: "mhub-752b1",
   storageBucket: "mhub-752b1.appspot.com",
-  messagingSenderId: FIREBASE_MESSAGE_ID,
-  appId: FIREBASE_APP_ID,
+  messagingSenderId: "1:900873241304:web:931deb336375b8ce90cdad",
+  appId: "900873241304",
   measurementId: "G-0VL6QH3TWM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export default app
+export default app;
